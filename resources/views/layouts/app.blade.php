e256596fc036a5a5315d95a90b4f6f1904be791f
VasilMirov <58307550+VasilMirov@users.noreply.github.com>
2020-03-07 15:37:15 +0100

    Basic website with home page, contact page, about page. Created layouts and added a sidebar

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>Basic website</title>
+    </head>
+    <body>
+        @yield('content')
+
+        @include('include.sidebar')
+    </body>
+</html>
